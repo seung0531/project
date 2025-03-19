d5695ccafd25b167c09e0a8251a8040850ababf8
seung0531 <yunsh053113@gmail.com>
2025-03-19 14:02:40 +0900

    스크립트

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+const messageDiv = document.getElementById("message");
+
+button.addEventListener("click", function(){
+    document.body.style.backgroundColor = "lightblue";
+
+    messageDiv.textContent = "버튼이 클릭되었습니다!";
+});
